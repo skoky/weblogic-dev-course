4e48a7c421b77e6a98198c66715e6bd20923eb9b
Ladislav Skokan <ladislav.skokan@wincor-nixdorf.cz>
2015-03-07 13:48:02 +0100

    Updated logging app with several different log samples

--- logging/src/main/java/com/skoky/logging/LoggingServlet.java
+++ logging/src/main/java/com/skoky/logging/LoggingServlet.java
@@ -1,6 +1,9 @@
 package com.skoky.logging;
 
+import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import weblogic.logging.NonCatalogLogger;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,7 +11,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 
+gic.logging.LoggingHelper;
 
 @WebServlet("/log")
 public class LoggingServlet extends HttpServlet {
@@ -16,6 +21,7 @@ public class LoggingServlet extends HttpServlet {
     private final static Logger log4j = Logger.getLogger(LoggingServlet.class);
     private static final java.util.logging.Logger logJ =
             java.util.logging.Logger.getLogger(LoggingServlet.class.getSimpleName());
+    private static final NonCatalogLogger wlslogger = new NonCatalogLogger("MyPerfectApp");
 
 
     @Override
@@ -23,10 +29,20 @@ public class LoggingServlet extends HttpServlet {
                       HttpServletResponse response)
             throws ServletException, IOException {
 
+        ConsoleAppender ca = new ConsoleAppender();
+        ca.setWriter(new OutputStreamWriter(System.out));
+        ca.setLayout(new PatternLayout("%-5p [%t]: %m%n"));
+        log4j.addAppender(ca);
+
         String message = request.getParameter("message");
-        System.out.println("System out log:" + message);
+
+        System.out.println("System out log :" + message);
         log4j.debug("Log4j log:" + message);
-        logJ.finer("Java log:"+message);
+        logJ.severe("Java log:" + message);
+
+        wlslogger.warning("WLS log:"+message);
+
+        System.out.println("Done");
 
         getServletConfig().getServletContext().getRequestDispatcher("/").forward(request, response);
     }
